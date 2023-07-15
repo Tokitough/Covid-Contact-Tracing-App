5657e00306d7429d283e908a40d913fc27a3c302
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-07-16 01:16:53 +0800

    bare gui

--- contact_tracing_gui.py
+++ contact_tracing_gui.py
@@ -28,19 +28,19 @@ address_entry = Entry(parent)
 name.grid(row = 0, column = 0)
 name_entry.grid(row = 0, column = 1)
 
-gender.grid(row = 1, column = 1, pady = 10, padx = 5)
+gender.grid(row = 1, column = 0, pady = 10, padx = 5)
 gender_entry.grid(row = 1, column = 1)
 
-bday.grid(row = 2, column = 1, pady = 10, padx = 5)
+bday.grid(row = 2, column = 0, pady = 10, padx = 5)
 bday_entry.grid(row = 2, column = 1)
 
-phone_num.grid(row = 3, column = 1, pady = 10, padx = 5)
+phone_num.grid(row = 3, column = 0, pady = 10, padx = 5)
 phone_num_entry.grid(row = 3, column = 1)
 
-email_add.grid(row = 4, column = 1, pady = 10, padx = 5)
+email_add.grid(row = 4, column = 0, pady = 10, padx = 5)
 email_add_entry.grid(row = 4, column = 1)
 
-address.grid(row = 5, column = 1, pady = 10, padx = 5)
+address.grid(row = 5, column = 0, pady = 10, padx = 5)
 address_entry.grid(row = 5, column = 1)
 
 parent.mainloop()
