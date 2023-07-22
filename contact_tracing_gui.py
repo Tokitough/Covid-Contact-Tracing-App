4974c351e2bda908bfab66ce20d4e3b8fd3e41ce
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-07-22 23:31:29 +0800

    Fixed errors

--- contact_tracing_gui.py
+++ contact_tracing_gui.py
@@ -6,69 +6,70 @@ import tkinter as tk
 class ContactTracingGUI:
     def __init__(self, root):
         self.root = root
-    
-    # Create Labels
-    root = tk.Tk()
-    root.title("Covid Contact Tracing")
-    root.geometry("700x500")
-    name = Label(root, text = "Name: ")
-    gender = Label(root, text = "Gender: ")
-    bday = Label(root, text = "Birthday: ")
-    phone_num = Label(root, text = "Phone Number: ")
-    email_add = Label(root, text = "Email Address: ")
-    address = Label(root, text = "Address: ")
-    test = Label(root, text = "Have you tested for Covid? \n Select only one")
+        
+        # Create Labels
+        root = tk.Tk()
+        root.title("Covid Contact Tracing")
+        root.geometry("700x500")
+        name = Label(root, text = "Name: ")
+        gender = Label(root, text = "Gender: ")
+        bday = Label(root, text = "Birthday: ")
+        phone_num = Label(root, text = "Phone Number: ")
+        email_add = Label(root, text = "Email Address: ")
+        address = Label(root, text = "Address: ")
+        test = Label(root, text = "Have you tested for Covid? \n Select only one")
+        
+        # Create Entry Fields and Radiobuttons
+        self.name_entry = Entry(root)
+        self.gender_entry = Entry(root)
+        self.bday_entry = Entry(root)
+        self.phone_num_entry = Entry(root)
+        self.email_add_entry = Entry(root)
+        self.address_entry = Entry(root)
+        
+        self.test_var = IntVar()
+        test_1 = Radiobutton(root, text = "No", variable = self.test_var, value = 1)
+        test_2 = Radiobutton(root, text = "Yes(Negative)", variable = self.test_var, value = 2)
+        test_3 = Radiobutton(root, text = "Yes(Positive)", variable = self.test_var, value = 3)
+        test_4 = Radiobutton(root, text = "Yes(Pending)", variable = self.test_var, value = 4)
+        
+        # Place Labels and entry fields
+        name.grid(row = 0, column = 0, pady = 10, padx = 5)
+        self.name_entry.grid(row = 0, column = 1)
 
-    # Button
-    submit_btn = Button(root, text = "Submit Entry", command = self.add_entry())
-    submit_btn.place(x = 160, y = 350, width = 90)
-    
-    search_btn = Button(root, text = "Search Entry", command = self.search_entry())
-    search_btn.place(x = 160, y = 380, width = 90)
-    
-    # Create Entry Fields and Radiobuttons
-    name_entry = Entry(root)
-    gender_entry = Entry(root)
-    bday_entry = Entry(root)
-    phone_num_entry = Entry(root)
-    email_add_entry = Entry(root)
-    address_entry = Entry(root)
-    
-    test_var = IntVar()
-    test_1 = Radiobutton(root, text = "No", variable = test_var, value = 1)
-    test_2 = Radiobutton(root, text = "Yes(Negative)", variable = test_var, value = 2)
-    test_3 = Radiobutton(root, text = "Yes(Positive)", variable = test_var, value = 3)
-    test_4 = Radiobutton(root,text = "Yes(Pending)", variable = test_var, value = 4)
-    
-    # Place Labels and entry fields
-    name.grid(row = 0, column = 0, pady = 10, padx = 5)
-    name_entry.grid(row = 0, column = 1)
+        gender.grid(row = 1, column = 0, pady = 10, padx = 5)
+        self.gender_entry.grid(row = 1, column = 1)
 
-    gender.grid(row = 1, column = 0, pady = 10, padx = 5)
-    gender_entry.grid(row = 1, column = 1)
+        bday.grid(row = 2, column = 0, pady = 10, padx = 5)
+        self.bday_entry.grid(row = 2, column = 1)
 
-    bday.grid(row = 2, column = 0, pady = 10, padx = 5)
-    bday_entry.grid(row = 2, column = 1)
+        phone_num.grid(row = 3, column = 0, pady = 10, padx = 5)
+        self.phone_num_entry.grid(row = 3, column = 1)
 
-    phone_num.grid(row = 3, column = 0, pady = 10, padx = 5)
-    phone_num_entry.grid(row = 3, column = 1)
+        email_add.grid(row = 4, column = 0, pady = 10, padx = 5)
+        self.email_add_entry.grid(row = 4, column = 1)
 
-    email_add.grid(row = 4, column = 0, pady = 10, padx = 5)
-    email_add_entry.grid(row = 4, column = 1)
-
-    address.grid(row = 5, column = 0, pady = 10, padx = 5)
-    address_entry.grid(row = 5, column = 1)
+        address.grid(row = 5, column = 0, pady = 10, padx = 5)
+        self.address_entry.grid(row = 5, column = 1)
+        
+        test.grid(row = 6, column = 0, pady = 10, padx = 5)
+        test_1.place(x = 160, y = 255)
+        test_2.place(x = 160, y = 275)
+        test_3.place(x = 160, y = 295)
+        test_4.place(x = 160, y = 315)
+        
+        # Button
+        self.submit_btn = tk.Button(root, text = "Submit Entry", command = self.add_entry)
+        self.submit_btn.place(x = 160, y = 350, width = 90)
     
-    test.grid(row = 6, column = 0, pady = 10, padx = 5)
-    test_1.place(x = 160, y = 255)
-    test_2.place(x = 160, y = 275)
-    test_3.place(x = 160, y = 295)
-    test_4.place(x = 160, y = 315)
+        self.search_btn = tk.Button(root, text = "Search Entry", command = self.search_entry)
+        self.search_btn.place(x = 160, y = 380, width = 90)
     
-    # Instance for contact tracing app
-    contact_trace = ContactTracing()
+        # Instance for contact tracing app
+        self.contact_trace = ContactTracing()
     
-    root.mainloop()
+        root.mainloop()
+        
     # Function for getting entries
     def add_entry(self):
         name = self.name_entry.get()
@@ -96,7 +97,7 @@ class ContactTracingGUI:
     # Search Entry
     def search_entry(self):
         
-        search_window = Toplevel(root)
+        search_window = Toplevel()
         name = self.name_entry.get()
         
         # Call search entry method from ContactTracing class
